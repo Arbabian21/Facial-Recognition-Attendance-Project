ecd226c8ef15f0680b2563186bd4352a6e830f36
Arbab <arifarbab21@gmail.com>
2025-09-03 16:23:02 -0400

    finished project

--- AttendanceTracker.py
+++ AttendanceTracker.py
@@ -2,7 +2,9 @@ import cv2
 import numpy as np
 import face_recognition
 import os
+from datetime import datetime
 
+already_marked = set()
 # generate a list of encodings for all the students in a class using their picture
 PATH = 'ImagesStudents'
 VALID_EXTS = {".jpg", ".jpeg", ".png", ".bmp", ".webp"}  # NEW
@@ -44,7 +46,31 @@ def getEncodings(images):
     return encodedList
 
 def markAttendance(name):
-    pass
+    folder = "Attendance"
+    today = datetime.today().date().isoformat()
+    file = f'{today}.csv'
+    full_path = os.path.join(folder, file)
+
+    os.makedirs(folder, exist_ok=True) # Ensure the folder exists
+    with open(full_path, "a+", encoding="utf-8") as f:
+        f.seek(0)
+        lines = f.readlines()
+        nameList = []
+        
+        if not lines:
+            f.write("Name,Time\n")
+            f.flush()
+            lines = ["Name,Time\n"]
+            
+        for line in lines[1:]:
+            entry = line.split(',')
+            nameList.append(entry[0])
+        
+        if name not in nameList:
+            now = datetime.now()
+            dtString = now.strftime('%H:%M:%S')
+            f.writelines(f'{name},{dtString}\n')
+    
 
 print("Beginning Encoding")
 encodeListKnownFaces = getEncodings(images)
@@ -75,21 +101,27 @@ while True:
     for encodedFace, faceLocation in zip(encodeCurrFrame, facesInFrame):
         matches = face_recognition.compare_faces(encodeListKnownFaces, encodedFace)
         faceDistance = face_recognition.face_distance(encodeListKnownFaces, encodedFace)
-        print(faceDistance)
+
         matchIndex = np.argmin(faceDistance)
         
         if matches[matchIndex]:
             name = studentNames[matchIndex].upper()
-            print(name)
+            trueName = studentNames[matchIndex]
+            
             y1,x2,y2,x1 = faceLocation
             y1,x2,y2,x1 = y1*4,x2*4,y2*4,x1*4
             cv2.rectangle(img,(x1,y1),(x2,y2), (0,255,0), 2)
             cv2.rectangle(img, (x1,y2-35),(x2,y2),(0,255,0), cv2.FILLED)
             cv2.putText(img, name, (x1+6, y2-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 2)
+            if trueName not in already_marked:
+                markAttendance(trueName)
+                already_marked.add(trueName)
             
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
     
     cv2.imshow('Webcam', img)
-    cv2.waitKey(1)
-    
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+capture.release()
+cv2.destroyAllWindows()    
